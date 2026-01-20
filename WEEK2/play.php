b5c63f81eb32c06723f1e4e0b9cc27627f0ffde5
chocomintyyy <20dit24f1068@pmu.mypolycc.edu.my>
2026-01-20 15:16:51 +0800

    third commit

--- WEEK2/play.php
+++ WEEK2/play.php
@@ -2,4 +2,7 @@
 $name = "Aten";
 echo "hello world $name. ";
 echo 'This is a full stack web development '.$name;
+echo "<h1> My name is Fatin Syazwani </h1>";
+echo "<h2>My PA name is Miss Athirah </h2>";
+echo "<h3>JTMK </h3>";
 ?>
