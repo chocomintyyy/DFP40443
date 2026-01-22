1fc8cdb05a4e6d2ccba8e945288076b9e86ada4c
chocomintyyy <20dit24f1068@pmu.mypolycc.edu.my>
2026-01-22 11:13:27 +0800

    Add files via upload

---
+++ WEEK2/bmi-calculator.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+    <head>
+</head>
+
+<body>
+    <form method="post" action = "<?php echo $_SERVER('PHP_SELF'); ?>">
+<label>Height (cm)</label>
+<input name="height">
+<label>Weight (kg)</label>
+<input name="weight">
+<input type="submit" value = "'Calculate">
+</form>
+<h1> Hello World</h1>
+</body>
+</html>
